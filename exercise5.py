1f43c7b7c73e8f86a6324455275b7ab6c595ebb6
Tim407-3D <timeeadragomir1@gmail.com>
2026-03-06 17:35:54 +0200

    updates

--- exercise5.py
+++ exercise5.py
@@ -1,23 +1,41 @@
 data = {"Name": [], "Age": [], "Salary": []}
-while True:
-    newName = str(input("Enter name: "))
-    if newName.isalpha():
-        break
-    else:
-        print("Error: Name must contain only letters. Try again.")
-while True:
-    newAge = input("Enter age: ")
-    if newAge.isnumeric() and int(newAge)>0:
-        break
-    else:
-        print("Error: Age must be a positive number. Try again.")
-while True:     
-    newSalary =input("Enter salary: ")
-    if newSalary.isnumeric() and int(newSalary)>0:
-        break
-    else:
-        print("Error: Salary must be a positive number. Try again.")
+history = []
+def dataUpdate():
 
-data["Name"].append(newName)
-data["Age"].append(newAge)
-data["Salary"].append(newSalary)
+    while True:
+        newName = str(input("Enter name: "))
+        if newName.isalpha():
+            break
+        else:
+            print("Error: Name must contain only letters. Try again.")
+
+    while True:
+        newAge = input("Enter age: ")
+        if newAge.isnumeric() and int(newAge)>=16 and int(newAge)<65:
+            break
+        else:
+            print("Error: Age must be a positive number between 16 and 65. Try again.")
+
+    while True:     
+        newSalary =input("Enter salary: ")
+        if newSalary.isnumeric() and int(newSalary)>0:
+            break
+        else:
+            print("Error: Salary must be a positive number. Try again.")
+
+    result= {"Name": newName, "Age": newAge, "Salary": newSalary}
+    return result
+
+
+
+while True:
+    choices= input("Enter 'r' to run the function or 'q' to quit: ")
+    if choices =="r"or choices=="R":
+       result= dataUpdate()
+       history.append(result)
+    if choices =="q" or choices=="Q":
+       break
+
+
+for person in history:
+    print(f"Name: {person['Name']}, Age: {person['Age']}, Salary: ${person['Salary']}")
