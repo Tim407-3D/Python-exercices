db79cd89db5c3dbc1c1fe7f59e7b31dd45d7c8e8
Tim407-3D <timeeadragomir1@gmail.com>
2026-03-09 22:46:56 +0200

    updates

--- Personal Exercises/Lab1OperatiiCuDateNumerice/venitulNet.py
+++ Personal Exercises/Lab1OperatiiCuDateNumerice/venitulNet.py
@@ -3,4 +3,8 @@ venitUtilizator=int(input("Introduceti venitul: "))
 cheltuieliUzuale=50/100* venitUtilizator
 celtuieliRecreere=30/100* venitUtilizator
 economii=20/100* venitUtilizator
-print(f"\n Venit: {venitUtilizator}\n Recomandarile Noastre: \n Cheltuieli uzuale: {cheltuieliUzuale}\n Recreere: {celtuieliRecreere}\n Economii si datorii: {economii}")
+print(f"""Venit: {venitUtilizator}
+    Recomandarile Noastre:
+    Cheltuieli uzuale: {cheltuieliUzuale}
+    Recreere: {celtuieliRecreere} 
+    Economii si datorii: {economii}""")
