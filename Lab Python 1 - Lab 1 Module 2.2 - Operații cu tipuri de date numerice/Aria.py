3ca32cc68dd5b899aacc0d5c2412b50c259799fe
Tim407-3D <timeeadragomir1@gmail.com>
2026-03-09 13:01:19 +0200

    Updates

---
+++ Lab Python 1 - Lab 1 Module 2.2 - Operații cu tipuri de date numerice/Aria.py
@@ -0,0 +1,24 @@
+date= {"Baze": [] ,"Height": [] }
+
+def updateData(arie):
+
+    while True:
+        newBase= (input ("Enter the base of the Triangle:"))
+        if newBase.isnumeric:
+            break
+        else:
+            print ("Error the value is not numeric.Try again.")
+
+
+    while True:
+        newHeigth=(input("enter the Height of the Triangle:"))
+        if newHeigth.isnumeric:
+            break
+        else:
+            Print("Error the value is not numeric.Try again.")
+
+    arie=1/2* newBase*newHeigth
+    result=arie
+
+
+
